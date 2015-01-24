5af1478ac7aa09daaf1e649041bb8c9c6ffa47d4
unknown <malejandro@GIS-DEV06.gistecnologias.com>
2015-01-24 12:23:13 -0600

    * Rock behavior created and complete       - Pending animation * Fixed problems on level 3

---
+++ Idea/Assets/Scripts/Level 3/RockBehaviourScript.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RockBehaviourScript : MonoBehaviour
+{
+    public float Lifes;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if ((Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began))
+        {
+
+            RaycastHit2D hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint((Input.GetTouch(0).position)), Vector2.zero);
+            if (hit.collider != null)
+            {
+                PunchRock();
+            }
+        }
+       
+    }
+
+    void OnMouseDown()
+    {
+            PunchRock();
+    }
+
+    void PunchRock()
+    {
+        Lifes--;
+        if (Lifes <= 0)
+            Destroy(this.gameObject);
+       
+        Debug.Log("Touched it");
+    }
+}
