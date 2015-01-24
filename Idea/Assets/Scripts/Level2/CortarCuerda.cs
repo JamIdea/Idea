9d87b05fbb53864e779b5ff6fea88c3a3c3bfeb7
OscarNavarroVital <oscar.navarro@hotmail.com>
2015-01-24 16:06:30 -0600

    Level 2
    Assets y animaciones para nivel 2

---
+++ Idea/Assets/Scripts/Level2/CortarCuerda.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CortarCuerda : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnMouseEnter()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            //this.transform.rigidbody2D.gravityScale = 1;
+            Destroy(this.gameObject.GetComponent<HingeJoint2D>());
+            //Destroy(this.gameObject.hingeJoint);
+            //Destroy(this.gameObject);
+        }
+    }
+}
