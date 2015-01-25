057d609576114e0bd7df43679a822b97100e8cac
Frank Gonzalez <adrfrank@outlook.com>
2015-01-24 21:30:21 -0600

    Revert "merge"
    This reverts commit 72fd61f2712a3f76c2a185ecf36a789236c0c3ee.

--- Idea/Assets/Scripts/Level4/SceneManagerScript.cs
+++ Idea/Assets/Scripts/Level4/SceneManagerScript.cs
@@ -3,61 +3,55 @@ using System.Collections;
 
 public class SceneManagerScript : MonoBehaviour {
 
-	public float Step;
-    public bool IsMoving;
-    public float MaxPosition;
-    public float MinPosition;
-
 	public GameObject Player;
-	public GameObject Nut;
+	public GameObject Rock;
 	public float velocity;
 	private bool attachedToCamera;
-
+	private Vector3 OldPosition;
 	// Use this for initialization
-	void Start () {
-		StartCoroutine ("BeginPlayerAnimation");
-		attachedToCamera = false;
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		if (attachedToCamera && Player.GetComponent<PlayerBehaviourScript>().Moving)
-		{
-			AttachCameraToPlayer();
-		}
-		else if (!attachedToCamera)
-		{
-			AttachCameraToNut();
-		}
+	void Start()
+	{
+		OldPosition = Rock.transform.localPosition;
+		attachedToCamera = true;
+		StartCoroutine("BeginPlayerAnimation");
 	}
 	
 	IEnumerator BeginPlayerAnimation()
 	{
 		Debug.Log("begin");
-		Player.GetComponent<PlayerBehaviourScript>().Moving = false;
-		yield return new WaitForSeconds(3);
-		//Player.GetComponent<Animator>().Play("ScaredPlayer");
-
+		yield return new WaitForSeconds(1);
+		Player.GetComponent<Animator>().Play("ScaredPlayer");
 		Debug.Log("wait 1");
+		Player.GetComponent<PlayerBehaviourScript>().Moving = false;
+		yield return new WaitForSeconds(2);
+		AttachCameraToRock();
+		Player.GetComponent<Animator>().Play("RuningPlayer");
+		Debug.Log("WAIT 2");
+		attachedToCamera = false;
+		Rock.rigidbody2D.gravityScale = 0.2f;
 		Player.GetComponent<PlayerBehaviourScript>().Moving = true;
-		attachedToCamera = true;
-		//Player.GetComponent<Animator>().Play("RuningPlayer");
-
 	}
 	
-	private void AttachCameraToPlayer()
+	private void AttachCameraToRock()
 	{
-		if (Player != null)
-		{
-			Camera.main.transform.position = Vector3.Lerp(Camera.main.transform.position, Player.transform.position, Time.deltaTime);
-		}
+		Camera.main.transform.position = Vector3.Lerp(Camera.main.transform.position, Rock.transform.position + new Vector3(-.04f, -.04f, -10f), Time.deltaTime);
+		Camera.main.orthographicSize = Mathf.Lerp(Camera.main.orthographicSize, 10f, Time.deltaTime);
+		//Camera.main.transform.localPosition = new Vector3(Rock.transform.localPosition.x, Rock.transform.localPosition.y, Camera.main.transform.localPosition.z);
 	}
-
-	private void AttachCameraToNut()
+	
+	// Update is called once per frame
+	void Update()
 	{
-		if (Nut != null)
+		if (attachedToCamera && Player.GetComponent<PlayerBehaviourScript>().Moving)
 		{
-			Camera.main.transform.position = Vector3.Lerp(Camera.main.transform.position, Nut.transform.position, Time.deltaTime);
+			var tmpPosition = Camera.main.transform.localPosition;
+			tmpPosition.x += velocity;
+			Camera.main.transform.localPosition = tmpPosition;
 		}
+		else if (!attachedToCamera)
+		{
+			AttachCameraToRock();
+		}
+		
 	}
 }
