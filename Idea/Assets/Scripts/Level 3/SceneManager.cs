b51147052b809c20bc11a0dcf172cb25aa5b21f5
unknown <malejandro@GIS-DEV06.gistecnologias.com>
2015-01-24 18:16:18 -0600

    changing game translation to forces

--- Idea/Assets/Scripts/Level 3/SceneManager.cs
+++ Idea/Assets/Scripts/Level 3/SceneManager.cs
@@ -5,16 +5,23 @@ public class SceneManager : MonoBehaviour
 {
 
     public GameObject Player;
+    public GameObject Hand;
     public GameObject Nut;
     public float velocity;
+    public float position;
     private bool attachedToCamera;
-    private Vector3 OldPosition;
     // Use this for initialization
     void Start()
     {
-        OldPosition = Nut.transform.localPosition;
         attachedToCamera = true;
-        StartCoroutine("BeginPlayerAnimation");
+        position = Camera.main.transform.localPosition.x - Player.transform.localPosition.x;
+
+    }
+
+    void OnTriggerEnter2D(Collider2D element)
+    {
+        if (element.name == "Player")
+            StartCoroutine("BeginPlayerAnimation");
     }
 
     IEnumerator BeginPlayerAnimation()
@@ -23,14 +30,19 @@ public class SceneManager : MonoBehaviour
         yield return new WaitForSeconds(1);
         Player.GetComponent<Animator>().Play("ScaredPlayer");
         Debug.Log("wait 1");
-        Player.GetComponent<PlayerBehaviourScript>().Moving = false;
+        Player.GetComponent<PlayerBehaviourScript>().IsRunning = false;
+        Player.rigidbody2D.velocity = new Vector2(0f, 0f);
         yield return new WaitForSeconds(2);
         AttachCameraToRock();
         Player.GetComponent<Animator>().Play("RuningPlayer");
         Debug.Log("WAIT 2");
         attachedToCamera = false;
         Nut.rigidbody2D.gravityScale = 0.2f;
-        Player.GetComponent<PlayerBehaviourScript>().Moving = true;
+        Player.rigidbody2D.gravityScale = 0.00001f;
+        Player.transform.Rotate(new Vector3(0, 0, 22f));
+        yield return new WaitForSeconds(2.5f);
+        Destroy(Hand);
+        Player.GetComponent<PlayerBehaviourScript>().IsRunning = true;
     }
 
     private void AttachCameraToRock()
@@ -51,10 +63,10 @@ public class SceneManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (attachedToCamera && Player.GetComponent<PlayerBehaviourScript>().Moving)
+        if (attachedToCamera && Player.GetComponent<PlayerBehaviourScript>().IsRunning)
         {
             var tmpPosition = Camera.main.transform.localPosition;
-            tmpPosition.x += velocity;
+            tmpPosition.x = Player.transform.localPosition.x + position;
             Camera.main.transform.localPosition = tmpPosition;
         }
         else if (!attachedToCamera)
